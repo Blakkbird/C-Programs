bf1ef37a60db7c246a5b53384675d6b20e46ecf6
Blakkbird <ssgurkirpa@outlook.com>
2024-10-28 13:10:31 +0530

    Update swapwords.c
    Tried this today after a while. I think it simplifies it a bit?

--- swapwords.c
+++ swapwords.c
@@ -1,53 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char string[100],secondwrd[100],thirdwrd[100];
-    puts("Enter a string");
-    gets(string);
-    int len=strlen(string);
-    int count=0,i=0,k=0,m=0,start2,end2,start3,end3,str2,str3;
-    while(string[i]!=' '){
-        count++;
-        i++;
-    }
-    start2=count+1;
-    str2=start2;
-    count=0;
-    while (string[str2]!=' ')
+int main() // app(1)as(2)as(3)ehe
+{
+    char sent[100];
+    printf("Enter sentence: \n");
+    fgets(sent, 100, stdin);
+
+    int len = strlen(sent);
+    int w2s = -1, w2e = -1, w3e = -1, c = 0;
+
+    for (int i = 0; i < len; i++)
     {
-        count++;
-        str2++;
-    }
-    end2=str2-1;
-    start3=str2+1;
-    str3=start3;
-    count=0;
-    while(string[str3]!=' '){
-        count++;
-        str3++;
-    }
-    end3=str3-1;
-    for(int j=start2;j<=end2;j++){
-        secondwrd[k++]=string[j];
-    }
-    secondwrd[k]='\0';
-    for(int n=start3;n<=end3;n++){
-        thirdwrd[m++]=string[n];
-    }
-    thirdwrd[m]='\0';
-    for(int j=0;j<len;j++){
-        if(j==start2){
-            printf("%s",thirdwrd);
-            j=end2;
-        }
-        else if(j==start3){
-            printf("%s",secondwrd);
-            j=end3;
-        }
-        else{
-            printf("%c",string[j]);
+        if (sent[i] == ' ')
+        {
+            c++;
+            if (c == 1) w2s = i;
+            if (c == 2) w2e = i;
+            if (c == 3) w3e = i;
         }
     }
+
+    if (w2s == -1 || w2e == -1 || w3e == -1) {
+        printf("Input sentence must have at least three words.\n");
+        return 1;
+    }
+
+    
+    for (int i = 0; i < w2s; i++) // Print first word
+    {
+        printf("%c", sent[i]);
+    }
+
+    printf(" ");
+    
+    for (int i = w2e + 1; i < w3e; i++) // Print third word
+    {
+        printf("%c", sent[i]);
+    }
+
+    printf(" ");
+
+    
+    for (int i = w2s + 1; i < w2e; i++) // Print second word
+    {
+        printf("%c", sent[i]);
+    }
+
+    for (int i = w3e; i < len; i++)
+    {
+        printf("%c", sent[i]);
+    }
+
     return 0;
 }
